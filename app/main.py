427efbdc68f87c05f8f2d05575c19ba4f0877037
Andrii Omelchenko <andreyomem@gmail.com>
2025-08-05 15:37:57 +0300

    fix flake8 app

--- app/main.py
+++ app/main.py
@@ -12,13 +12,16 @@ KEY = os.getenv("API_KEY")
 
 def get_weather() -> None:
     result = requests.get(URL + f"q={CITY}&key={KEY}")
-    if  result.status_code == 200:
+    if result.status_code == 200:
         data = result.json()
         city = data["location"]["name"]
         temp = data["current"]["temp_c"]
         humidity = data["current"]["humidity"]
         condition = data["current"]["condition"]["text"]
-        full_info = f"City: {city}, Temperature: {temp}, Humidity: {humidity},Condition: {condition}"
+        full_info = (f"City: {city}, "
+                     f"Temperature: {temp}, "
+                     f" Humidity: {humidity}, "
+                     f"Condition: {condition}")
         print(full_info)
 
 
