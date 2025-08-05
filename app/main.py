d13343f0b797c282be10e1d96cdac234b6abcf55
Andrii Omelchenko <andreyomem@gmail.com>
2025-08-05 15:34:08 +0300

    add Dockerfile implement get_weather()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = "https://api.weatherapi.com/v1/current.json?"
+CITY = "Paris"
+KEY = os.getenv("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(URL + f"q={CITY}&key={KEY}")
+    if  result.status_code == 200:
+        data = result.json()
+        city = data["location"]["name"]
+        temp = data["current"]["temp_c"]
+        humidity = data["current"]["humidity"]
+        condition = data["current"]["condition"]["text"]
+        full_info = f"City: {city}, Temperature: {temp}, Humidity: {humidity},Condition: {condition}"
+        print(full_info)
 
 
 if __name__ == "__main__":
